89728900b867621333901d62e4b5eb9bb9095342
KELECHIDAVIS <kboss16058@gmail.com>
2023-12-06 13:27:04 -0800

    Insert and create finished

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Node.h"
+
+Node* createNode ( int data ); 
+Node* insert(Node* root, int  data); 
+void deleteNode (Node* root, int data); 
+void printBFS(Node* root); // level traversal 
+void printDFS(Node* root); // depth traversal 
+
+
+int main () 
+{
+
+
+    return 0 ; 
+}
+
+Node* createNode ( int data )
+{
+    Node* newNode = new Node(); 
+    newNode->data = data ; 
+    return newNode ; 
+}
+
+Node* insert(Node* root, int  data)
+{
+    if(root == nullptr)
+        return createNode(data); 
+    
+    if(data>=root->data)
+    {
+        root->right = insert(root->right, data); 
+    }else{
+        root->left = insert(root->left, data ) ; 
+    }
+    return root; 
+}
