3a71d9a31743ed95ce9c4c95096bf66de1d6e722
KELECHIDAVIS <kboss16058@gmail.com>
2023-12-06 18:34:14 -0800

    Finished printing

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include "Node.h"
+#include <queue>
+using namespace std;  
 
 Node* createNode ( int data ); 
 Node* insert(Node* root, int  data); 
-void deleteNode (Node* root, int data); 
+Node* deleteNode (Node* root, int data); 
 
 
 // preorder , postorder, level order, in order traversal (11/27 lec)
-void printBFS(Node* root); // level traversal 
-void printDFS(Node* root); // depth traversal 
+void printLevelOrder(Node* root); //breadth first 
+void printInOrder(Node* root ); 
+void printPreOrder(Node* root ); 
+void printPostOrder(Node* root ); 
 
 
 int main () 
 {
-
+    cout <<"First"<<endl; 
 
     return 0 ; 
 }
@@ -39,7 +43,71 @@ Node* insert(Node* root, int  data)
     return root; 
 }
 
-void deleteNode (Node* root, int data)
+//replace the node with it's inorder predecessor 
+Node* deleteNode (Node* root, int data)
 {
+    // im going to use the binary search deletion function 
 
+    if( root == nullptr) 
+        return root ; 
+
+    return root;  
+}
+
+void printPreOrder(Node* root )
+{
+    if(root == nullptr)
+        return; 
+
+    cout<<root->data<<" "<<endl; // visit node 
+
+    printPreOrder(root->left); //left
+
+    printPreOrder(root->right); //right 
+
+} 
+void printInOrder(Node* root )
+{
+    if(root == nullptr)
+        return; 
+
+
+    printPreOrder(root->left); //left
+
+    cout<<root->data<<" "<<endl; // visit node 
+
+    printPreOrder(root->right); //right 
+
+}  
+void printPostOrder(Node* root )
+{
+    if(root == nullptr)
+        return; 
+
+
+    printPreOrder(root->left); //left
+    printPreOrder(root->right); //right 
+    cout<<root->data<<" "<<endl; // visit node 
+}
+
+void printLevelOrder(Node* root)
+{
+    queue<Node*> nodeQueue; 
+
+    nodeQueue.push(root); 
+
+    while(!nodeQueue.empty())
+    {
+        Node* current = nodeQueue.front(); 
+        nodeQueue.pop(); 
+
+        cout<<current->data<<endl; 
+
+        // add children  
+        if(current->left)
+            nodeQueue.push(current->left); 
+        if(current->right)
+            nodeQueue.push(current->right); 
+
+    }
 }
